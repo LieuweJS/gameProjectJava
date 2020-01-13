db48e93d71916eb87bf6fdf37969581d225a74cb
Lieuwe Baron <46071445+LieuweJS@users.noreply.github.com>
2020-01-13 14:02:38 +0100

    update

--- Map.java
+++ Map.java
@@ -291,22 +291,22 @@ public class Map {
         int ChamberNumber;
         // ArrayList<Enemy> enemies = new ArrayList<Enemy>();
         // ArrayList<Item> items = new ArrayList<Item>();
-        //ArrayList<ArrayList<GroundType>> chamber = new ArrayList<new Arraylist<GroundType>()>();
+        ArrayList<ArrayList<GroundType>> chamber = new ArrayList<>();
         public Chamber(int ChamberNum, String[][] chamberMap) {
             for(int i = 0; i < 15; i++) {
                 for(int j = 0; j < 15; j++) {
                     if(chamberMap[i][j] == "W") {
-
+                        chamber.get(i).add(new GroundType("Wall"));
                     } else if(chamberMap[i][j] == "O") {
-
+                        chamber.get(i).add(new GroundType("Open"));
                     } else if(chamberMap[i][j] == "C") {
-
+                        chamber.get(i).add(new GroundType("Chest"));
                     } else if(chamberMap[i][j] == "P") {
-
+                        chamber.get(i).add(new GroundType("PreviousFloorDoor"));
                     } else if(chamberMap[i][j] == "N") {
-
+                        chamber.get(i).add(new GroundType("NextFloorDoor"));
                     } else if(chamberMap[i][j] == "E") {
-                    
+                        chamber.get(i).add(new GroundType("Enemy"));
                     }
                 }
             }
